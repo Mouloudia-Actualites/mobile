b77edb9d18823202197d53881f62345b33b074cf
Mouloudia-Actualites <igmooran213@gmail.com>
2024-08-14 03:38:41 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,53 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope);
+// service-worker.js
+
+// Cache name
+const CACHE_NAME = 'my-cache-v1';
+
+// Files to cache
+const URLs_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
+  // Add more files as needed
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(URLs_TO_CACHE);
       })
-      .catch(error => {
-        console.log('Service Worker registration failed:', error);
-      });
-  });
-}
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return the cached response if found, otherwise fetch from the network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // Delete old caches
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
